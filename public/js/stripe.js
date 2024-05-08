f92e58d0937cbdbd9cec7dee01f8bc6ff8628964
Nguyen Trung Duc <ntduc2810@gmail.com>
2024-05-08 22:34:52 +0700

    initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+import { loadStripe } from '@stripe/stripe-js';
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1. Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
+    );
+
+    // 2. Create checkout form + charge credit card
+    const stripe = await loadStripe(
+      'pk_test_51PC0dKRurF39Vs71009w521eqjsAhfJ6qNo47GfSLGhrnZjIJvBzv5kG1isSxXovFGWG1WYmLzAjv1WhFrOkc1S600D24P2G4i',
+    );
+    stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    showAlert('error', err);
+  }
+};
